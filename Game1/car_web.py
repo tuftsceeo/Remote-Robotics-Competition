ad1056246d8e137f5db34ea5d359789e10a7c187
William Goldman <60369048+iliketocode2@users.noreply.github.com>
2025-07-06 12:48:43 -0400

    final version?

--- Game1/car_web.py
+++ Game1/car_web.py
@@ -1,15 +1,20 @@
 import json
 
-# async def myCallback(message):
-#     value = json.loads(message.decode())
+controller_data = {"x": 0, "y": 0}
+print("Starting...")
 
-def fred(message):
-    print(message['payload'])
-    try: 
-        await myBle.send_str(str(message['payload']))
-    except Exception as e:
-        print("ERROR: ", e)
+async def fred(message):
+    payload = message['payload']
+    payload_dict = json.loads(payload)
+    topic = payload_dict['topic']
+    value = payload_dict['value']
     
-# myBle.callback = myCallback        
+    if topic == "/Controller/x":
+        controller_data["x"] = value
+    elif topic == "/Controller/y":
+        controller_data["y"] = value
+    
+    combined_data = json.dumps(controller_data)
+    await myBle.send_str(combined_data)
+    
 myChannel.callback = fred
-   
