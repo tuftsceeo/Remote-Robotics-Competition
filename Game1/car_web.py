017ebd7002433d9295776c2b21c873a7d26b3f64
William Goldman <60369048+iliketocode2@users.noreply.github.com>
2025-07-06 11:09:45 -0400

    Update car_web.py

--- Game1/car_web.py
+++ Game1/car_web.py
@@ -1,17 +1,20 @@
 import json
 
-async def myCallback(message):
-    value = json.loads(message.decode())
+# async def myCallback(message):
+#     value = json.loads(message.decode())
 
 def fred(message):
     payload = message['payload']
     payload_dict = json.loads(payload)
     topic = payload_dict['topic']
     value = payload_dict['value']
-    # print('RECEIVED:', topic, value)
-    if (topic == "/Will/Spike"):
+    if (topic == "/Controller/x"):
+        # await myBle.send_str({'x':str(value)})
+        await myBle.send_str(str(value))
+    if (topic == "/Controller/y"):
+        # await myBle.send_str({'y':str(value)})
         await myBle.send_str(str(value))
     
-myBle.callback = myCallback        
+# myBle.callback = myCallback        
 myChannel.callback = fred
    
