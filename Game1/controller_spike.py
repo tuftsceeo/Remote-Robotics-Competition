602164d298fd6d8805844b1267da2128988ecef8
William Goldman <60369048+iliketocode2@users.noreply.github.com>
2025-07-06 20:04:35 -0400

    Update controller_spike.py

--- Game1/controller_spike.py
+++ Game1/controller_spike.py
@@ -7,18 +7,14 @@ import json
 
 def grabData():
     (x,y,z)= motion_sensor.acceleration() #The values are mili G, so 1 / 1000 G
-    message = {'x':x, 'y':y, 'z':z}
+    message = {'x':x, 'y':y}
     return str(json.dumps(message))
-
-def callback(data):
-        print(data.decode())
 
 def peripheral(name): 
     try:
         print('waiting ...')
         p = Yell(name, interval_us=30000, verbose = False)
         if p.connect_up():
-            p.callback = callback
             print('Connected')
             time.sleep(1)
             while p.is_connected:
