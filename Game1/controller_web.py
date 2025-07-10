735b5c81c647df0ccb03c3516cec7b33736dbde3
William Goldman <60369048+iliketocode2@users.noreply.github.com>
2025-07-10 15:21:22 -0400

    Update controller_web.py

--- Game1/controller_web.py
+++ Game1/controller_web.py
@@ -2,7 +2,7 @@ import json
 
 async def myCallback(message):
     value = json.loads(message.decode())
-    await myChannel.post('/Controller/x', value['x'])
-    await myChannel.post('/Controller/y', value['y'])
+    controller_data = { 'x': value['x'], 'y': value['y'] }
+    await myChannel.post('/Controller/data', controller_data)
 
 myBle.callback = myCallback
