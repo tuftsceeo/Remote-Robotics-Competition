98388ca8b53e2994d2acbbaa4d6c40b6ae04c206
William Goldman <60369048+iliketocode2@users.noreply.github.com>
2025-07-14 08:50:27 -0400

    Update controller_web.py

--- Game1/talking_to_a_hub/controller_web.py
+++ Game1/talking_to_a_hub/controller_web.py
@@ -5,10 +5,8 @@ import json
 await w.feed_rate(40)
 
 async def grab(message):
-    # value = message['IMU']['yaw']
-    controller_data = { 'x': message['x'], 'y': message['y'] }
+    controller_data = { 'x': message['IMU']['roll'], 'y': message['IMU']['pitch'] }
     if myChannel.is_connected:
-        # await myChannel.post('/SPIKE/yaw',value)
         await myChannel.post('/Controller/data', controller_data)
 
 w.final_callback = grab
