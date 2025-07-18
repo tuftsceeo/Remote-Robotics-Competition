d9cf961ccd8fde41ba4d284e9436e23a6a267165
William Goldman <60369048+iliketocode2@users.noreply.github.com>
2025-07-17 23:12:23 -0400

    50ms delay

--- Game1/OpenMV/streaming/main_code.py
+++ Game1/OpenMV/streaming/main_code.py
@@ -5,11 +5,9 @@ import ubinascii
 import gc
 import websocket
 
-import secret
-
 # WiFi credentials
-WIFI_SSID = secret.wifi['username']
-WIFI_PASSWORD = secret.wifi['password']
+WIFI_SSID = ""
+WIFI_PASSWORD = ""
 
 # WebSocket server details
 WS_HOST = "chrisrogers.pyscriptapps.com"
@@ -25,21 +23,21 @@ def init_camera():
     sensor.reset()
     sensor.set_pixformat(sensor.RGB565)
     sensor.set_framesize(sensor.QVGA)
-    
+
     sensor.set_auto_gain(True)  #False, gain_db=10)
     sensor.set_auto_exposure(True)  #False, exposure_us=8000)
     sensor.skip_frames(time=500)
-    
+
 def snap():
     try:
         img = sensor.snapshot()
         jpeg_bytes = img.compress(quality=45)
         base64_str = ubinascii.b2a_base64(jpeg_bytes).decode('utf-8').strip()
-        
+
         #print(f"Image: {len(jpeg_bytes)}B -> {len(base64_str)}B base64")
         print('.',end = '')
         return base64_str
-        
+
     except Exception as e:
         print(f"Image capture error: {e}")
         return None
@@ -49,19 +47,19 @@ wlan = network.WLAN(network.STA_IF)
 def connect_wifi():
     print("Connecting to WiFi...")
     wlan.active(True)
-    
+
     if not wlan.isconnected():
         wlan.connect(WIFI_SSID, WIFI_PASSWORD)
-        
+
         timeout = 10000
         start_time = time.ticks_ms()
-        
+
         while not wlan.isconnected():
             if time.ticks_diff(time.ticks_ms(), start_time) > timeout:
                 print("WiFi timeout!")
                 return False
-            time.sleep_ms(200)
-    
+            time.sleep_ms(50)
+
     print(f"WiFi connected: {wlan.ifconfig()}")
     return True
 
@@ -70,13 +68,13 @@ def main():
     if not connect_wifi():
         print("WiFi connection failed!")
         return
-    
+
     try:
         counter = 0
         while True:
             if not websock.check():
                 continue
-            
+
             base64_image = snap()
             if base64_image:
                 image_message = {
@@ -85,25 +83,25 @@ def main():
                     "timestamp": time.ticks_ms()
                 }
                 #print(f"Sending image ...")
-                
+
                 if not websock.send(image_message):
                     print(f"Failed to send image ")
                     websock.ssl_sock.close()
                     websock.ssl_sock = None
                     continue
-            
+
             # Wait between images
             time.sleep(wait_time)
             counter += 1
             if not (counter % 10): # garbage collect every 10 iterations
                 gc.collect()
-    
+
     except KeyboardInterrupt:
         print("Stopping transmission...")
-    
+
     except Exception as e:
         print(f"Main error: {e}")
-    
+
     finally:
         if websock.ssl_sock:
             try:
