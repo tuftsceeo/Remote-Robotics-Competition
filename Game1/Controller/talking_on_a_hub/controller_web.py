e593efa0e179e1cb92ab163bbee9cfba499e1735
William Goldman <60369048+iliketocode2@users.noreply.github.com>
2025-07-21 11:32:37 -0400

    Update controller_web.py

--- Game1/Controller/talking_on_a_hub/controller_web.py
+++ Game1/Controller/talking_on_a_hub/controller_web.py
@@ -1,8 +1,13 @@
+import asyncio
 import json
 
-async def myCallback(message):
-    value = json.loads(message.decode())
-    controller_data = { 'x': value['x'], 'y': value['y'] }
-    await myChannel.post('/Controller/data', controller_data)
+await w.feed_rate(100)
 
-myBle.callback = myCallback
+async def grab(message):
+    value1 = message.get('IMU', {}).get('roll')
+    value2 = message.get('IMU', {}).get('yaw')
+    controller_data = {'x': value1, 'y': value2}
+    if myChannel.is_connected:
+        await myChannel.post('/Controller/data', controller_data)
+
+w.final_callback = grab
